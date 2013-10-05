010097c244529a8905b34a32756b4a4f07c1ee6d
Andre Schumacher <schumach@icsi.berkeley.edu>
2013-10-04 18:20:44 -0700

    added files for Spark 0.8

---
+++ spark-0.8/spark-base/files/spark-env.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+export SCALA_HOME=/opt/scala-2.9.3
+export SPARK_HOME=__SPARK_HOME__
+export SPARK_WORKER_CORES=1
+export SPARK_MEM=700m
+export SPARK_WORKER_MEMORY=700m
+export SPARK_WORKER_CORES=1
+export HADOOP_HOME="/etc/hadoop"
+export MASTER="spark://__MASTER__:7077"
+export SPARK_MASTER_IP=__MASTER__
+#export SPARK_LOCAL_IP=__LOCAL_IP__
+SPARK_JAVA_OPTS="-Dspark.local.dir=/tmp/spark "
+SPARK_JAVA_OPTS+="-Dspark.kryoserializer.buffer.mb=10 "
+SPARK_JAVA_OPTS+="-verbose:gc -XX:-PrintGCDetails -XX:+PrintGCTimeStamps "
+export SPARK_JAVA_OPTS
+export JAVA_HOME=__JAVA_HOME__
