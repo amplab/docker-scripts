56fc116b1a2e4e6ce6eb52299ebb49caab2cc2b4
Andre Schumacher <schumach@icsi.berkeley.edu>
2013-10-10 12:50:21 -0700

    adding a -v option to deploy script in order to mount host directory to containers /data

--- shark/deploy/start_shark_cluster.sh
+++ shark/deploy/start_shark_cluster.sh
@@ -6,7 +6,7 @@ MASTER_IP=
 # starts the Shark master container
 function start_shark_master() {
     echo "starting Shark master container"
-    MASTER=$(sudo docker run -i -t -d -dns $NAMESERVER_IP -h master shark-master:$SHARK_VERSION)
+    MASTER=$(sudo docker run -i -t -d -dns $NAMESERVER_IP -h master $VOLUME_MAP shark-master:$SHARK_VERSION)
     echo "started master container:      $MASTER"
     sleep 3
     MASTER_IP=$(sudo docker logs $MASTER 2>&1 | egrep '^MASTER_IP=' | awk -F= '{print $2}' | tr -d -c "[:digit:] .")
@@ -19,7 +19,7 @@ function start_shark_workers() {
     for i in `seq 1 $NUM_WORKERS`; do
 	echo "starting Shark worker container"
 	hostname="worker${i}"
-	WORKER=$(sudo docker run -d -dns $NAMESERVER_IP -h $hostname shark-worker:${SHARK_VERSION} ${MASTER_IP} spark://${MASTER_IP}:7077)
+	WORKER=$(sudo docker run -d -dns $NAMESERVER_IP -h $hostname $VOLUME_MAP shark-worker:${SHARK_VERSION} ${MASTER_IP} spark://${MASTER_IP}:7077)
 	echo "started worker container    $WORKER"
 	sleep 3
 	WORKER_IP=$(sudo docker logs $WORKER 2>&1 | egrep '^WORKER_IP=' | awk -F= '{print $2}' | tr -d -c "[:digit:] .")
@@ -37,6 +37,8 @@ function print_cluster_info() {
     echo "visit Spark WebUI at:       http://$MASTER_IP:8080/"
     echo "visit Hadoop Namenode at:   http://$MASTER_IP:50070"
     echo "ssh into master via:        ssh -i $BASEDIR/apache-hadoop-hdfs-precise/files/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${MASTER_IP}"
+    echo ""
+    echo "/data mapped:               $VOLUME_MAP"
     echo ""
     echo "kill cluster via:           sudo docker kill $MASTER"
     echo "***********************************************************************"
