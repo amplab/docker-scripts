ddb6664a049302396e8502df6f0d5a9a67ff67aa
Andre Schumacher <schumach@icsi.berkeley.edu>
2013-10-17 21:03:17 -0700

    changes to build scripts

--- build/tag_all.sh
+++ build/tag_all.sh
@@ -5,13 +5,13 @@ if [[ "$USER" != "root" ]]; then
     exit 1
 fi
 
-image_list=( "spark-master:0.8.0" "spark-worker:0.8.0" "spark-shell:0.8.0" "shark-master:0.7.3" "shark-worker:0.7.3" "shark-shell:0.7.3" )
+image_list=( "spark-master:0.8.0" "spark-worker:0.8.0" "spark-shell:0.8.0" "shark-master:0.7.0" "shark-worker:0.7.0" "shark-shell:0.7.0" )
 
 IMAGE_PREFIX="amplab/"
 
 # NOTE: the order matters but this is the right one
 for i in ${image_list[@]}; do
 	image=$(echo $i | awk -F ":" '{print $1}')
-        echo docker tag ${IMAGE_PREFIX}${i} ${image} latest
-	docker tag ${IMAGE_PREFIX}${i} ${image} latest
+        echo docker tag ${IMAGE_PREFIX}${i} ${IMAGE_PREFIX}${image} latest
+	docker tag ${IMAGE_PREFIX}${i} ${IMAGE_PREFIX}${image} latest
 done
