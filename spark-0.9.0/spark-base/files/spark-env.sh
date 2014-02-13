999a1eb2460e3e2de8b5722e7d491cbd2e63007c
Xuefeng.Wu <benewu@gmail.com>
2014-02-13 14:03:46 +0800

    add spark-0.9.0 docker file

---
+++ spark-0.9.0/spark-base/files/spark-env.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+export SCALA_HOME=/opt/scala-2.9.3
+export SPARK_HOME=__SPARK_HOME__
+export SPARK_WORKER_CORES=1
+export SPARK_MEM=800m
+export SPARK_WORKER_MEMORY=1500m
+export SPARK_MASTER_MEM=1500m
+export SPARK_MASTER_IP=__MASTER__
+export HADOOP_HOME="/etc/hadoop"
+export MASTER="spark://__MASTER__:7077"
+export SPARK_LOCAL_DIR=/tmp/spark
+#SPARK_JAVA_OPTS="-Dspark.local.dir=/tmp/spark "
+#SPARK_JAVA_OPTS+=" -Dspark.akka.logLifecycleEvents=true "
+#SPARK_JAVA_OPTS+="-Dspark.kryoserializer.buffer.mb=10 "
+#SPARK_JAVA_OPTS+="-verbose:gc -XX:-PrintGCDetails -XX:+PrintGCTimeStamps "
+#export SPARK_JAVA_OPTS
+#SPARK_DAEMON_JAVA_OPTS+=" -Dspark.akka.logLifecycleEvents=true "
+#export SPARK_DAEMON_JAVA_OPTS
+export JAVA_HOME=__JAVA_HOME__
